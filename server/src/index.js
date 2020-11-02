3e6c6c343049b976cfbb890f8e2605648532eaba
FrederikHSt <frederikhstaal@gmail.com>
2020-11-02 20:51:35 +0100

    Update index.js
    test commit

--- server/src/index.js
+++ server/src/index.js
@@ -3,3 +3,5 @@ const port = process.env.PORT || 8080;
 const createServer = require("./server");
 createServer().then(server => server.listen(port, 
   () => console.log(`${appName} running on port ${port}!`)));
+
+  //HELLO
